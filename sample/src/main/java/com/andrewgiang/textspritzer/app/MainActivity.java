272c36fc669a93e2e11bd1a6ea982058ab8e42d5
Andrew Giang <agiang021@gmail.com>
2014-03-11 10:29:58 -0400

    Remove Unused Menu's

--- sample/src/main/java/com/andrewgiang/textspritzer/app/MainActivity.java
+++ sample/src/main/java/com/andrewgiang/textspritzer/app/MainActivity.java
@@ -2,8 +2,6 @@ package com.andrewgiang.textspritzer.app;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.SeekBar;
 
 import com.andrewgiang.textspritzer.lib.SpritzerTextView;
@@ -72,26 +70,5 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 }
