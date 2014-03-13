984bebbac0563a7bb45cc0757309e8cd98a0fdf8
Andrew Giang <agiang021@gmail.com>
2014-03-13 00:34:22 -0400

    update sample to include SpritzerDialogFragment

--- sample/src/main/java/com/andrewgiang/textspritzer/app/MainActivity.java
+++ sample/src/main/java/com/andrewgiang/textspritzer/app/MainActivity.java
@@ -1,14 +1,23 @@
 package com.andrewgiang.textspritzer.app;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.SeekBar;
 
+import com.andrewgiang.textspritzer.lib.SpritzerDialogFragment;
 import com.andrewgiang.textspritzer.lib.SpritzerTextView;
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getName();
+    public static final String INPUT = "OpenSpritz has nothing to do with Spritz Incorporated. " +
+            "This is an open source, community created project, made with love because Spritz is " +
+            "such an awesome technique for reading with.";
     private SpritzerTextView mSpritzerTextView;
     private SeekBar mSeekBarTextSize;
     private SeekBar mSeekBarWpm;
@@ -18,10 +27,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mSpritzerTextView = (SpritzerTextView) findViewById(R.id.spritzTV);
-        mSpritzerTextView.setSpritzText("OpenSpritz has nothing to do with Spritz Incorporated. " +
-                "This is an open source, community created project, made with love because Spritz is " +
-                "such an awesome technique for reading with.");
+        mSpritzerTextView.setSpritzText(INPUT);
         mSeekBarTextSize = (SeekBar) findViewById(R.id.seekBarTextSize);
         mSeekBarWpm = (SeekBar) findViewById(R.id.seekBarWpm);
 
@@ -72,4 +80,35 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_launch_dialog:
+                showSpritzerDialog();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showSpritzerDialog() {
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment prev = getSupportFragmentManager().findFragmentByTag("SpritzerDialog");
+        if (prev != null) {
+            ft.remove(prev);
+        }
+        ft.addToBackStack(null);
+
+        final SpritzerDialogFragment fragment = SpritzerDialogFragment.newInstance(INPUT);
+        fragment.show(getSupportFragmentManager(), "SpritzerDialog");
+    }
 }
