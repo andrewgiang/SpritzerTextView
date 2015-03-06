b199df6681c21bc4a3f7a6601d551bb528cca9d2
Michal Tajchert <michal.tajchert@polidea.com>
2015-03-06 14:37:22 +0100

    Fix for imports.

--- lib/src/main/java/com/andrewgiang/textspritzer/lib/Spritzer.java
+++ lib/src/main/java/com/andrewgiang/textspritzer/lib/Spritzer.java
@@ -1,10 +1,12 @@
 package com.andrewgiang.textspritzer.lib;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.os.Message;
 import android.text.Spannable;
 import android.text.SpannableString;
-import android.text.style.TextAppearanceSpan;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
